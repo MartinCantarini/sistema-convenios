2787e83ae1933480d1ce5c4145afb8e541a0ad87
Martin Daniel Cantarini <md.cantarini@gmail.com>
2016-02-21 15:10:45 -0300

    prueba1

--- db/migrate/20160107123011_change_type_fechafirma_to_agreement.rb
+++ db/migrate/20160107123011_change_type_fechafirma_to_agreement.rb
@@ -1,16 +1,9 @@
 class ChangeTypeFechafirmaToAgreement < ActiveRecord::Migration
   def up
-<<<<<<< HEAD
   	remove_column :agreements, :fechadefirma
-=======
-    remove_column :agreements, :fechadefirma
   end
 
   def down
     add_column :agreements, :fechadefirma, :date
->>>>>>> 0321bd0703b1d3f4ea8b16f500c6d5f87a592747
-  end
-  def down
-  	add_column :agreements, :fechadefirma, :date
   end	
 end
